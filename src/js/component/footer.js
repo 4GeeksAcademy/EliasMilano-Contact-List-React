0f03c3477db38f2d74b6874d92dbc0094b024b13
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-11-04 17:29:32 +0000

    added images for initial contacts

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer text-secondary mt-3 py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			Made with <i className="fa fa-heart text-success" /> by{" "}
 			<span>MilanoDev</span>
 		</p>
 	</footer>
